a9ccb9032a53fb035cae385e6bfa50f92d80a5d3
thefluffyoshi <54290315+thefluffyoshi@users.noreply.github.com>
2020-09-28 13:28:04 -0700

    on screen2 navigates to home

--- app/src/main/java/com/example/countingapp/Screen2.java
+++ app/src/main/java/com/example/countingapp/Screen2.java
@@ -3,6 +3,7 @@ package com.example.countingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class Screen2 extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class Screen2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen2);
+    }
+
+    public void onClickBack(View view0)
+    {
+        finish();
     }
 }
