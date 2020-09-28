f83f2d1ce6d1491b711ea44cec24f10c8764916f
thefluffyoshi <54290315+thefluffyoshi@users.noreply.github.com>
2020-09-28 13:09:40 -0700

    First commit

---
+++ app/src/main/java/com/example/countingapp/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.countingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.i("info", "-------This message is in the log------");
+        Log.e("error", "---that's an error----");
+        Log.w("warning", "-----this is a warning-----");
+        Log.wtf("What a terrible failure", "-----This is bad-----");
+
+        Toast.makeText(this, "Your toast is ready!", Toast.LENGTH_LONG).show();
+    }
+
+    public void onClickFirstButton(View view)
+    {
+        Toast.makeText(this, "You clicked the first button", Toast.LENGTH_LONG).show();
+    }
+
+    public void onClickSecondButton(View view)
+    {
+        Toast.makeText(this, "You clicked the second button", Toast.LENGTH_LONG).show();
+    }
+}
