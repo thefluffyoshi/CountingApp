0bca6abe376abde15f35e7e43c9734d40e334554
thefluffyoshi <54290315+thefluffyoshi@users.noreply.github.com>
2020-09-28 13:41:53 -0700

    Can now count up

--- app/src/main/java/com/example/countingapp/MainActivity.java
+++ app/src/main/java/com/example/countingapp/MainActivity.java
@@ -2,14 +2,17 @@ package com.example.countingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    int num1 = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,16 +28,32 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClickFirstButton(View view)
     {
-        Toast.makeText(this, "You clicked the first button", Toast.LENGTH_LONG).show();
+        num1++;
+        TextView introTV = (TextView) findViewById(R.id.introTV); // name of the TV to be updated
+        introTV.setText("You now have " + num1 + " fishes");
+        //Toast.makeText(this, "You clicked the first button", Toast.LENGTH_LONG).show();
     }
 
     public void onClickSecondButton(View view)
     {
-        Toast.makeText(this, "You clicked the second button", Toast.LENGTH_LONG).show();
+        num1--;
+        //Toast.makeText(this, "You clicked the second button", Toast.LENGTH_LONG).show();
     }
 
     public void onClickGoToScrnTwo (View view)
     {
         startActivity(new Intent(MainActivity.this, Screen2.class));
     }
+
+//    public void openDialogue(View view)
+//    {
+//        AlertDialog.Builder builder = AlertDialog.Builder(this);
+//        builder.setMessage("Confirm reset count");
+//        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialogInterface, int i) {
+//                Toast.makeText(MainActivity.this, "Yessir!", Toast.LENGTH_LONG).show();
+//            }
+//        })
+//    }
 }
